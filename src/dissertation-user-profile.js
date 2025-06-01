24aa5dc7180bcdf7b35fa5448b003923a8a1ac77
leslie628 <lf24aap@herts.ac.uk>
2025-06-01 23:13:54 +0100

    test commit

--- src/dissertation-user-profile.js
+++ src/dissertation-user-profile.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
-
+//hello
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
