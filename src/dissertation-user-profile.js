f58125e949ce9200f7ddeaa7fd41ab8a5f217789
leslie628 <lf24aap@herts.ac.uk>
2025-06-01 23:15:34 +0100

    removed comment

--- src/dissertation-user-profile.js
+++ src/dissertation-user-profile.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
-//hello
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
